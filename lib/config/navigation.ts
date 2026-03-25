40de4e76977171e7a0ba2c8d3602376c4057858d
LeonardoGomezz <leonardogomez306@gmail.com>
2026-03-25 11:54:18 -0500

    feat: update navigation routes

--- lib/config/navigation.ts
+++ lib/config/navigation.ts
@@ -1,9 +1,10 @@
-import { Users, MapPin, Tv } from "lucide-react";
+import { Users, MapPin, Tv, LayoutDashboard } from "lucide-react";
 import { NavItem } from "@/types";
 
 export const NAVIGATION_CONFIG = {
   ITEMS: [
-    { icon: Users, label: "Characters", id: "characters", href: "/" },
+    { icon: LayoutDashboard, label: "Dashboard", id: "dashboard", href: "/" },
+    { icon: Users, label: "Characters", id: "characters", href: "/characters" },
     { icon: MapPin, label: "Locations", id: "locations", href: "/locations" },
     { icon: Tv, label: "Episodes", id: "episodes", href: "/episodes" },
   ] as NavItem[],
